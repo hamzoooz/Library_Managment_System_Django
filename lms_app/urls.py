2982feff1336372a36c4f524c900b5a209ded5b6
hamzoooz <hamzoooz123@gmail.com>
2022-07-29 06:03:15 +0200

    Library Manage

---
+++ lms_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import  path
+# from django.urls import urlpatterns, path
+from django.urls import URLPattern, path
+from . import views
+
+
+# URLPatterns = [
+urlpatterns = [
+    path("", views.index , name="index"),
+]
