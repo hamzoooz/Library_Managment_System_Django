3823f72e75647c604d375285ec93107a4bbc4acf
حمزه بانقا <68731690+hamzoooz@users.noreply.github.com>
2022-07-30 03:42:49 -0400

    fix
    fix

--- lms_app/urls.py
+++ lms_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import  path
 # from django.urls import urlpatterns, path
-from django.urls import URLPattern, path
+# from django.urls import URLPattern, path
 from . import views
 
 
 # URLPatterns = [
 urlpatterns = [
-    path("", views.index , name="index"),
+    path('', views.index , name="index"),
 ]
