6bfce88d14dc5e83b6a505379526ba9e692a2d6b
hamzoooz <hamzoooz123@gmail.com>
2024-02-07 09:17:34 +0300

    init

--- lms_app/models.py
+++ lms_app/models.py
@@ -21,15 +21,21 @@ class Book(models.Model):
     
     title        = models.CharField(max_length=50)
     auther       = models.CharField(max_length=250, null=True, blank=True)
-    # price        = models.DecimalField(max_digits=7 ,decimal_places=2, null=True, blank=True )
-    # photo_book   = models.ImageField(upload_to='photo', null=True, blank=True, default='/media/photo/photo1.png')
-    photo_auther = models.ImageField(upload_to='photo', null=True, blank=True )
-    pages        = models.IntegerField(null=True, blank=True )
-    retal_price_day = models.DecimalField(max_digits=7 ,decimal_places=2, null=True, blank=True )
-    retal_period = models.IntegerField(null=True, blank=True )
+    shelf       = models.CharField(max_length=250, null=True, blank=True)
+    locations       = models.CharField(max_length=250, null=True, blank=True)
+    price        = models.DecimalField(max_digits=7 ,decimal_places=2, null=True, blank=True)
+    photo_book   = models.ImageField(upload_to='photo', null=True, blank=True, default='/media/photo/photo1.png')
+    photo_auther = models.ImageField(upload_to='photo', null=True, blank=True)
+    pages        = models.IntegerField(null=True, blank=True)
+    retal_price_day = models.DecimalField(max_digits=7 ,decimal_places=2, null=True, blank=True)
+    retal_period = models.IntegerField(null=True, blank=True)
     active       = models.BooleanField(default=True)
-    status       = models.CharField(max_length=50, choices = status_book,null=True, blank=True  )
-    category     = models.ForeignKey(Category, on_delete=models.PROTECT  , null=True, blank=True  )
+    status       = models.CharField(max_length=50, choices = status_book,null=True, blank=True)
+    category     = models.ForeignKey(Category, on_delete=models.PROTECT  , null=True, blank=True)
+    
+    number_of_rental = models.IntegerField(null=True, blank=True)
+    number_of_download = models.IntegerField(null=True, blank=True)
+    number_of_view = models.IntegerField(null=True, blank=True)
     
     def __str__(self):
         return self.title
